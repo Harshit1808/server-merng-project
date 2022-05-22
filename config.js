9f20779014e61c86c9f11aaba2d4104fad7d1ee4
Harshit <harshitmarothiya1808@gmail.com>
2022-05-21 16:09:35 -1100

    my project

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  MONGODB:
+    'mongodb://saini1808:RBJgpG58FmiXsOID@cluster0-shard-00-00.znniu.mongodb.net:27017,cluster0-shard-00-01.znniu.mongodb.net:27017,cluster0-shard-00-02.znniu.mongodb.net:27017/?ssl=true&replicaSet=atlas-kycpih-shard-0&authSource=admin&retryWrites=true&w=majority',
+  SECRET_KEY: 'some very secret key'
+};
+//RBJgpG58FmiXsOID
